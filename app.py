44a0fe6f458c0b4e14167b9a4b3367c6735d5064
+ <+>
2024-01-25 18:17:31 +0300

    more with cors

--- app.py
+++ app.py
@@ -115,9 +115,9 @@ def sherehe_by_id(id):
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://icssherehe.netlify.app')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    response.headers['Access-Control-Allow-Origin'] = 'https://icssherehe.netlify.app'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
     return response
 
 if __name__ == "__main__":
